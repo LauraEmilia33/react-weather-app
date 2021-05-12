ec1a01046ccedf71b41f448067354837b95c5856
Lola Owns <73960435+LauraEmilia33@users.noreply.github.com>
2021-05-12 14:17:27 +0200

    Added the Footer
    -Included the GitHub Repository URL
    -Included my LinkedIn Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer className="Footer">
+        This project was coded by {" "}
+        <a
+          href="https://www.linkedin.com/in/laura-emilia-barojas/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Laura Barojas
+        </a> and it is {" "}
+        <a
+          href="https://github.com/LauraEmilia33/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
