c6a39a59d5b458d817d2395e44692190a4a72516
Lola Owns <73960435+LauraEmilia33@users.noreply.github.com>
2021-08-02 10:32:38 +0200

    Last but not final touches

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -13,6 +13,14 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "95f162609dd8746fc4a9169098a143e3"
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -33,13 +41,7 @@ export default function WeatherForecast(props) {
     );
 
   } else {
-    let apiKey = "95f162609dd8746fc4a9169098a143e3"
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
+    load();
     return null;
   }
 
